d78f4702e49650a69e27340e9a78b8ca69008fe1
Eric <makingentropy@gmail.com>
2017-08-19 00:52:08 -0500

    Updated basic file structure, added seedController (still needs finalized  collection names), normalize, basic server.js code, index.html.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+const plannerController = require('./controllers/eventmaker.js');
+app.use('/planner', plannerController);
+const seedController= require('./controllers/seedController.js');
+app.use('/seed', seedController);
+
+mongoose.connect('mongodb://localhost:27017/plannercrud');
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongo');
+});
+
+app.listen(3000, ()=>{
+    console.log('listening');
+});
