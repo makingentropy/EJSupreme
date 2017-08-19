6cf30de3e4029733a8a27f5bfac4af244a437603
Eric <makingentropy@gmail.com>
2017-08-19 13:32:19 -0500

    added models

---
+++ models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const usersSchema = mongoose.Schema({
+  email: String,
+  name: String,
+  age: Number,
+  phone: Number,
+  country: String,
+  state: String,
+  city: String,
+  zip: Number,
+  imagelink: String,
+  interests: Array,
+  eventsAttending: Array,
+  eventsOrganing: Array
+});
+
+const Events = mongoose.model('User', usersSchema);
+
+module.exports = Users;
