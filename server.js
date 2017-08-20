afb06880472588c53d9cb9f54ccb3e8ecef22767
Eric <makingentropy@gmail.com>
2017-08-20 13:01:36 -0500

    added session controller, required/used express-sessions in server

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json()); //creates a property on request called req.body
 app.use(express.static('public'));
+app.use(session({ //creates a property on request called req.session
+	  secret: "this is a random string secret", //a random string do not copy this value or your stuff will get hacked
+	  resave: false,
+	  saveUninitialized: false
 
+}));
 
 
 mongoose.connect('mongodb://localhost:27017/plannercrud');
