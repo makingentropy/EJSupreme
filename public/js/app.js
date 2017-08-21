0ded2875a61ba59e598e8a5dcfa7fceec29b0f84
Susan <cho@Choui-MacBook-Pro-164.local>
2017-08-21 09:17:13 -0400

    Fully operational CRUD, v2

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,99 @@
 const app=angular.module("MyApp",[]);
 
-app.controller("NavController",[function(){
-  this.collapseLogin=true;
-  this.collapseRegister=true;
+app.controller('mainController', ['$http', function($http){
+  const controller = this;
+  this.indexEditForm = 1;
+
+  this.getEvents = function(){
+    $http({
+      method: 'GET',
+      url: '/events'
+    }).then(function(response){
+      console.log(response, ' -- > this is the response');
+      controller.events = response.data;
+    }, function(error){
+      console.log('error');
+    });
+  },
+  this.postEvent = function(){
+    $http({
+      method: 'POST',
+      url: '/events',
+      data: {
+        title: this.title,
+        ownerEmail: this.ownerEmail,
+        date: this.date,
+        time: this.time,
+        imagelink: this.imagelink,
+        description: this.description,
+        requiredCost: this.requiredCost,
+        country: this.country,
+        state: this.state,
+        city: this.city,
+        zip: this.zip,
+        address: this.address,
+        interestTags: this.interestTags,
+      }
+    }).then(function(response){
+      console.log(response);
+      controller.getEvents();
+      controller.title = "";
+      controller.ownerEmail = "";
+      controller.date = "";
+      controller.time = "";
+      controller.imagelink = "";
+      controller.description = "";
+      controller.requiredCost = "";
+      controller.country = "";
+      controller.state = "";
+      controller.city = "";
+      controller.zip = "";
+      controller.address = "";
+      controller.interestTags = "";
+    }, function(error){
+      console.log('error');
+    });
+  },
+  this.editEvent = function(event){
+    $http({
+      method: 'PUT',
+      url: '/events/' + event._id,
+      data: {
+        title: this.updatedTitle
+      }
+    }).then(function(response){
+      console.log(response);
+      controller.getEvents();
+      controller.title = "";
+      controller.ownerEmail = "";
+      controller.date = "";
+      controller.time = "";
+      controller.imagelink = "";
+      controller.description = "";
+      controller.requiredCost = "";
+      controller.country = "";
+      controller.state = "";
+      controller.city = "";
+      controller.zip = "";
+      controller.address = "";
+      controller.interestTags = "";
+    }, function(error){
+      console.log('error');
+    });
+  },
+  this.deleteEvent = function(event){
+    $http({
+      method: 'DELETE',
+      url: '/events/' + event._id,
+      data: {
+        title: this.title
+      }
+    }).then(function(response){
+      console.log(response);
+      controller.getEvents();
+    }, function(error){
+      console.log('error');
+    });
+  }
+  this.getEvents();
 }]);
