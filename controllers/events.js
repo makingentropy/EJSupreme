9b6191449940512213dd63d18e196cc2845fce36
Susan <sungjamesjoo@gmail.com>
2017-08-23 23:43:03 -0400

    Syntax

--- controllers/events.js
+++ controllers/events.js
@@ -9,10 +9,9 @@ router.get('/', (req, res)=>{
 });
 
 router.post('/', (req, res)=>{
-    console.log(req.body);
-    Events.create(req.body, (err, createdEvent)=>{
-      res.json(createdEvent);
-    });
+  Events.create(req.body, (err, createdEvent)=>{
+    res.json(createdEvent);
+  });
 });
 
 router.delete('/:id', (req, res)=>{
@@ -22,13 +21,8 @@ router.delete('/:id', (req, res)=>{
 });
 
 router.put('/:id', (req, res)=>{
-  console.log("req.session,line 25, events.js: ",req.session);
-
-  //Model.findById(id, [projection], [options], [callback])
   Events.findById(req.params.id,  (err, updatedEvent)=>{
-    console.log("updatedEvent,line 29, events.js: ",updatedEvent);
     if(updatedEvent.ownerEmail === req.session.email){
-      console.log("updatedEvent.ownerEmail === req.session.email: true");
       updatedEvent.interestTags.push(req.body.interestTags);
       Events.findByIdAndUpdate(req.params.id, updatedEvent, (err, updated)=>{
         res.json(updatedEvent);
